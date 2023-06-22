9f8e99c0863c820c4c2358dbb46f72c8744f30bf
vikas karotia <vkaskarotia@gmail.com>
2023-06-22 18:11:15 +0530

    img

--- src/app/Components/Products.js
+++ src/app/Components/Products.js
@@ -3,7 +3,7 @@ import { Avatar } from '@mui/material'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import React from 'react'
 import Rating from '@mui/material/Rating';
-
+import Image from 'next/image'
 function Products({image,title,price,rating}) {
     return (
         <div className='posts'>
@@ -11,7 +11,7 @@ function Products({image,title,price,rating}) {
                 <Avatar src= 'https://wellgroomedgentleman.com/media/images/Tony_Stark_Beard_with_Quiff_Hairstyle.width-800.jpg' />
                 <h3>Sagar</h3>
             </div>
-            <img className='post_img' src={image} alt="" />
+            <Image className='post_img' src={image} alt="" width={200} height={300}/>
             <h3>₹ {price}</h3>
             <Rating name="half-rating" defaultValue={rating} precision={0.5} />
             <h4>{title}</h4>
