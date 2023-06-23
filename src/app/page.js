db8be2a6950982ca245f5a3c27af67e337e3f580
vikas karotia <vkaskarotia@gmail.com>
2023-06-23 12:19:36 +0530

    Home

--- src/app/page.js
+++ src/app/page.js
@@ -1,102 +1,11 @@
-'use client'
-
-import React, { useEffect, useState } from 'react'
-import Products from '@/app/Components/Products'
-import HomeIcon from '@mui/icons-material/Home';
-import SearchIcon from '@mui/icons-material/Search';
-import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
-import { Avatar } from '@mui/material'
-import ReorderIcon from '@mui/icons-material/Reorder';
-
+import React from 'react'
+import Home from '@/app/Components/Home'
 function page() {
-   const [products, setProducts] = useState([])
-   const [filter, setFilter] = useState([])
-   
-   useEffect(() => {
-      const api = async () => {
-         try {
-            const res = await fetch('https://fakestoreapi.com/products/')
-            const Allproducts = await res.json();
-            setProducts(Allproducts)
-
-         } catch (error) {
-            console.log(error)
-         }
-      }
-      api()
-   }, [products])
-
-
-   const slider = ()=>{
-   
-      document.querySelector('.sidebar').style.display='block';
-   }
-   const slider1 = ()=>{
-      document.querySelector('.sidebar').style.display='none';
-   }
-  
-   const post = () => {
-      window.location.reload();
-   }
-
-   const filters = (e) => {
-      name = e.target.name;
-      const res = products.filter(products => products.category === name);
-      setFilter(res);
-   }
-
-   return (
-      <div className='home'>
-         <div onClick={slider} className='home_icon'>
-            <ReorderIcon />
-         </div>
-         <div className='sidebar'>
-            <div onClick={slider1} className='slider_icon'>
-               <ReorderIcon />
-            </div>
-            <div className='slider_opt'>
-               <Avatar src='https://wellgroomedgentleman.com/media/images/Tony_Stark_Beard_with_Quiff_Hairstyle.width-800.jpg' />
-               <h3>Sagar</h3>
-            </div>
-            <div onClick={post} className='slider_opt'>
-               <HomeIcon style={{ color: "white" }} />
-               <h4>Home</h4>
-            </div>
-            <div className='slider_opt'>
-               <SearchIcon style={{ color: "white" }} />
-               <h4>Search</h4>
-            </div>
-            <div onClick={post} className='slider_opt'>
-               <DynamicFeedIcon style={{ color: "white" }} />
-               <h4>Posts</h4>
-            </div>
-            <h3>Filter's</h3>
-            <div className='filters'>
-               <button onClick={filters} name="electronics" >electronics</button>
-               <button onClick={filters} name="jewelery">jewelery</button>
-               <button onClick={filters} name="men's clothing">men's clothing</button>
-               <button onClick={filters} name="women's clothing">women's clothing</button>
-            </div>
-         </div>
-
-         <div className='home_post'>
-
-            {
-               (filter.length !== 0) ?
-                  filter.map(product => (
-                     <div className='single'>
-                        <Products key={product.id} image={product.image} title={product.title} price={product.price} rating={product.rating.rate} />
-                     </div>
-                  )) : products.map(product => (
-                     <div key={product.id} className='single'>
-                        <Products image={product.image} title={product.title} price={product.price} rating={product.rating.rate} />
-                     </div>
-                  ))
-            }
-         </div>
-
-      </div>
-   )
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
 }
 
-export default page 
+export default page
