c8d1ef742dfec1fb51b7aa54443ab15c25e0ac8a
vikas karotia <vkaskarotia@gmail.com>
2023-06-23 16:12:08 +0530

    search

--- src/app/Components/Home.js
+++ src/app/Components/Home.js
@@ -11,8 +11,9 @@ import ReorderIcon from '@mui/icons-material/Reorder';
 function Home() {
    const [products, setProducts] = useState([])
    const [filter, setFilter] = useState([])
-
-   useEffect(() => {
+   const [input,setInput] = useState();
+   const [searchArray, setSearchArray] = useState([]);
+     useEffect(() => {
       const api = async () => {
          try {
             const res = await fetch('https://fakestoreapi.com/products/')
@@ -37,6 +38,12 @@ function Home() {
       window.location.reload();
    }
 
+   const handleInput = (e)=>{
+    setInput(e.target.value)
+   
+    const srch = products.filter(items=>items.title.toLowerCase().includes(e.target.value))
+    setSearchArray(srch);
+   }  
    const filters = (e) => {
       name = e.target.name;
       const res = products.filter(products => products.category === name);
@@ -45,13 +52,20 @@ function Home() {
 
    return (
       <div className='home'>
+        
+
          <div onClick={slider} className='home_icon'>
             <ReorderIcon />
          </div>
          <div className='sidebar'>
-            <div onClick={slider1} className='slider_icon'>
+         <div className='search_input'>
+         <div onClick={slider1} className='slider_icon'>
                <ReorderIcon />
             </div>
+         <SearchIcon style={{ color: "white" }} />
+         <input onChange={handleInput} type="text" placeholder='search products'/>
+         </div>
+            
             <div className='slider_opt'>
                <Avatar src='https://wellgroomedgentleman.com/media/images/Tony_Stark_Beard_with_Quiff_Hairstyle.width-800.jpg' />
                <h3>Sagar</h3>
@@ -78,18 +92,21 @@ function Home() {
         </div>
 
          <div className='home_post'>
-
             {
-               (filter.length !== 0) ?
+               (filter.length !== 0)?
                   filter.map(product => (
                      <div key={product.id} className='single'>
                         <Products key={product.id} image={product.image} title={product.title} price={product.price} rating={product.rating.rate} />
                      </div>
-                  )) : products.map(product => (
+                  )) : (searchArray.length==0)?products.map(product => (
                      <div key={product.id} className='single'>
                         <Products image={product.image} title={product.title} price={product.price} rating={product.rating.rate} />
                      </div>
-                  ))
+                  )): searchArray.map(product => (
+                    <div key={product.id} className='single'>
+                       <Products key={product.id} image={product.image} title={product.title} price={product.price} rating={product.rating.rate} />
+                    </div>
+                 ))
             }
          </div>
 
